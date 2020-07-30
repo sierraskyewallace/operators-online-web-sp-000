15657b57b141cfb695ba5e8dfe03519446d2db96
Sierra Wallace <sskwallace@gmail.com>
2020-07-30 04:30:11 +0000

    Done.

--- lib/operations.rb
+++ lib/operations.rb
@@ -1,12 +1,20 @@
 def unsafe?(speed)
-
+  if speed > 60 
+    return true
+    elsif speed < 40
+    return true
+  else
+    false
+  end
 end
+
 
 
 
 def not_safe?(speed)
-	
+  speed < 40 || speed > 60 ? true : false
 end
+
 	
 
 
